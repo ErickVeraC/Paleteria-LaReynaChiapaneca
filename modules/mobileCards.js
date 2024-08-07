87d26654d89c3e526dbc7fac102addfafaca807f
Erick Vera <erickverac90@gmail.com>
2024-08-07 14:35:20 -0600

    Cambiando el logotipo para hacerlo independiente

--- modules/mobileCards.js
+++ modules/mobileCards.js
@@ -1,8 +1,9 @@
 function initCarousel() {
-  const carouselElement = document.querySelector(".carousel");
+  const carouselElement = document.querySelector("#carouselExampleIndicators");
   if (carouselElement) {
     const carousel = new bootstrap.Carousel(carouselElement, {
-      interval: 30000, // Cambiar cada 30 segundos
+      interval: 3000, // Cambiar cada 3 segundos
+      ride: "carousel", // Para que comience automáticamente
     });
   } else {
     console.error("Elemento del carrusel no encontrado");
