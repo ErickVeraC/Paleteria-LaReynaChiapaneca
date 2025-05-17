9f43859e10b0a8a23e2b1a461a90ad90526e42e5
Erick Vera <erickverac90@gmail.com>
2025-05-17 15:54:40 -0600

    feat: Added 6 publish from facebook

--- modules/main.js
+++ modules/main.js
@@ -5,8 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function fetchRecentPosts() {
-  const accessToken =
-    "EAANZAfhJOkUkBOzQfZB7i0MjxAoVzMCLudI6pa0OjdYem1N8AOJs9PcUs1ZA73OBFGfAuzdhw3ZCkblZAVMDdTKH7EMQ8VQCc1ZCs5iwwffbmQK6OcUcNWqhUZA5KOZAgHvctWHZA179PEPn1h6aFnZAgc2L09VrAZBvAcGExuUmG6YjlZBIVIDuV5UVSZAcJdqZBkze3PC0zvFXz4Wx22gtba6CiJDucHH8wZD";
+  const accessToken = "";
   if (!accessToken) {
     console.error("Access token not found");
     return;
@@ -27,20 +26,4 @@ function fetchRecentPosts() {
       }
     }
   );
-}
-
-function renderPosts(posts) {
-  console.log("Rendering posts:", posts);
-  const postsContainer = document.getElementById("facebook-posts");
-  postsContainer.innerHTML = "";
-
-  posts.forEach((post) => {
-    const postElement = document.createElement("div");
-    postElement.className = "facebook-post";
-    postElement.innerHTML = `
-      <p>${post.message}</p>
-      <a href="https://www.facebook.com/${post.id}" target="_blank">Ver en Facebook</a>
-    `;
-    postsContainer.appendChild(postElement);
-  });
 }
